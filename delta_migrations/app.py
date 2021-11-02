bbafc9c9454502a052bf4f5a9b6782991a73aed8
jared.magrath <magrathj@tcd.ie>
2021-11-02 20:03:18 +0000

    added way to run migration scripts

--- delta_migrations/app.py
+++ delta_migrations/app.py
@@ -1,5 +1,7 @@
-import os
+import os    
+import glob
 import click
+from subprocess import call
 
 @click.command()
 @click.option(
@@ -10,3 +12,15 @@ import click
 def create_migration_dir(location):
     click.echo(f"Creating directory at {location}")
     os.makedirs(f"{location}/migrations", exist_ok=True)
+
+
+@click.command()
+@click.option(
+    "--location",
+    default="/tmp/delta_migrations",
+    help="This specifies the location where you are storing the migration scripts",
+)
+def run_migrations():
+    migration_scripts = glob.glob('*_migration_*.py')
+    for script in migration_scripts:
+        call(["python", script])
