2bdee2810c2ecf20ac80b1e1358c40ab648a1aab
jared.magrath <magrathj@tcd.ie>
2021-11-04 16:09:50 +0000

    put mnt instead of tmp

--- tests/test_schema_migration_helper.py
+++ tests/test_schema_migration_helper.py
@@ -9,7 +9,7 @@ from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 def test_create_table(spark):
     input = {
             'table_name': 'example1',
-            'path': '/mnt/bronze/delta/example1', 
+            'path': '/tmp/bronze/delta/example1', 
             'modify_type': 'create_table',
             'schema': StructType([StructField("script_name", StringType(), False), StructField("applied", TimestampType(), False)]),
             'partition_by': ["script_name"],
