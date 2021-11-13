2ce7a10676f97b4d828442d890565c112bee0e3c
Jared Magrath <magrathj@tcd.ie>
2021-11-13 15:30:31 +0000

    Adding template
    Added template to migration directory. So that after the directory is created, we import the delta migrations example files

--- delta_migrations/app.py
+++ delta_migrations/app.py
@@ -8,5 +8,15 @@ import click
     help="This specifies the location you want to create the directory",
 )
 def create_migration_dir(location):
+   
     click.echo(f"Creating directory at {location}")
     os.makedirs(f"{location}/migrations", exist_ok=True)
+    
+    click.echo(f"Add delta migration starter scripts to {location}")
+
+    zip_url = "https://github.com/magrathj/delta-migrations-template/archive/refs/heads/main.zip"
+    with urlopen(zip_url) as zip_resp:
+        with ZipFile(BytesIO(zip_resp.read())) as zip_file:
+            zip_file.extractall(location)
+            
+    click.echo(f"Completed delta migration setup")
